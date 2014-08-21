c858132997cdd21e3ca179e3b3093ee13f3c8017
Thomas Helzle <ThomasHelzle@users.noreply.github.com>
2014-08-21 14:57:28 +0200

    Tiny Typo

--- KeyFadr.control.js
+++ KeyFadr.control.js
@@ -8,6 +8,6 @@ load("Reloop.js");
 DRUMPADS = false;
 CNAME = "KeyFadr";
 
-host.defineController("Reloop", "KeyFadr", "1.0", "cb1eb430-23b4-11e4-8c21-0800200c9a66");
+host.defineController("ReLoop", "KeyFadr", "1.0", "cb1eb430-23b4-11e4-8c21-0800200c9a66");
 host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["Reloop KeyFadr"], ["Reloop KeyFadr"]);
